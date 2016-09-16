71371409ff47e1387347be0b6493ee5753e27df6
Max Winderbaum <maxwinderbaum@gmail.com>
2016-09-15 22:58:50 -0700

    Add ability to run tests concurrently in Chrome, Firefox, and Safari

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -21,7 +21,7 @@ const karmaConfig = {
     preprocessors: {
         [`${config.dir_test}/test-bundler.js`]: [`webpack`],
     },
-    browsers: [`PhantomJS`],
+    browsers: [`PhantomJS`, `Chrome`, `Firefox`, `Safari`],
     webpack: {
         devtool: `cheap-module-source-map`,
         resolve: Object.assign({}, webpackConfig.resolve, {
